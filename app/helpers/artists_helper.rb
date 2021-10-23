d240de916f660ed9d912bda3bbfdc9493ccb99e6
Christopher <christopher.black.2209@googlemail.com>
2021-10-24 00:56:41 +0100

    commit tree 83778ca48b074220fe9f5fffb0e640ea97fcec6d

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,24 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist_id)
+    #binding.pry
+    if song.artist && artist_id
+      @artist = Artist.find(artist_id)
+      hidden_field_tag "song[:artist_id]", @artist
+      @artist.name
+    else
+      select_tag "song[:artist_id]", options_from_collection_for_select(Artist.all, :id, :name, song.artist.id.to_i)
+
+    # if artist_id.nil?
+    #   select_tag "song[:artist_id]", options_from_collection_for_select(Artist.all, :id, :name, song.artist.id.to_i)
+    # else
+    #   @artist = Artist.find(artist_id)
+    #   hidden_field_tag "song[:artist_id]", @artist
+    #   @artist.name
+        
+    end
+  end 
+
 end
